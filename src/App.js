70bcbaf468dbc2d32ae04d58646de6551749f7a4
ApoorvDubey23 <apoorvd2301@gmail.com>
2024-10-25 22:47:37 +0530

    Luminuous_test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import 'chart.js/auto';
+import { Router,BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignupPage from './pages/Signup';
 function App() {
+  
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
